4c7a243d8033d651d370605700a78a2ff8a627b1
Arjun Marar <arjunvmarar@gmail.com>
2023-03-02 13:18:13 +1100

    input validation

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { GraphQLError } from "graphql";
 
+import { validateRegisterInput } from "../../utils/validators.js";
 import { SECRET_KEY } from "../../../config/config.js";
 import User from "../../models/User.js";
 
@@ -13,7 +15,29 @@ export default {
       info
     ) => {
       //Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new GraphQLError("Validation Error", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            exception: { errors },
+          },
+        });
+      }
       //Make sure user does not exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new GraphQLError("Username already exist!", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       //TODO Hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
---
+++ src/utils/validators.js
@@ -0,0 +1,27 @@
+const validateRegisterInput = (username, email, password, confirmPassword) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const emailRegEx =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!email.match(emailRegEx)) {
+      errors.email = "Email must be a valid email address";
+    }
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.password = "Password must match";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+export { validateRegisterInput };
